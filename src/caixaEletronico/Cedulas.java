4365fefea67ab977ea2b91b1a693097c8168fb61
Abraão Queiroz <abraaorqueiroz@gmail.com>
2020-12-28 19:40:47 -0300

    Atualização dos cometários

--- src/caixaEletronico/Cedulas.java
+++ src/caixaEletronico/Cedulas.java
@@ -4,29 +4,61 @@ class Cedulas {
 	
 	private int cedulas[];
 	
+/**
+ * 	No chamado dessa classe, e criado um vetor de 4 posicoes,
+ * onde cada indice do vetor corresponde a quantidade de notas disponivel no caixa,
+ * onde na posição 0 sao as notas de 10 reais,  e assim sucessivamente ate a posicao 3 do vetor,
+ * que armazena a quantidade de notras de 100 reais.
+ */
+	
 	Cedulas(){
-		cedulas = new int [4];  				//Cria um vetor de 4 posições, onde cada índice do vetor corresponde a quantidade de notas disponível no caixa
-		for (byte j=0; j<4; j++) 				//Onde na posição 0 são as notas de 10 reais,  e assim sucessivamente até a posição 3 do vetor, que armazena a quantidade de notras de 100 reais
-			cedulas[j]=0;						//Garante que na inicialização, todos os indíces do vetor armazena o valor 0
+		cedulas = new int [4];
+		for (byte j=0; j<4; j++)
+			cedulas[j]=0;
 		
 	}
 	
+/**
+ *	Metodo para inserção de notas no caixa,
+ *onde deve ser informado qual o indice em que sera incluido cedulas,
+ *e a quantidade a ser incluida
+ */
 	public void RegistrarNotas(int i, int qnt) {
-		cedulas[i]+=qnt;						//Método para inserção de notas no caixa, onde deve ser informado qual o índice em que será incluido cédulas, e a quantidade a ser incluída
+		cedulas[i]+=qnt;						//
 	}	
 	
+/**
+ * 	Metodo para retornar quantas cedulas tem disponivel de um determinado valor,
+ * definido pelo indice recebido
+ */
+	
 	public int RetornaQuantidade(int i) {		
-		return cedulas[i];						//Método para retornar quantas cédulas tem disponível de um determinado valor, definido pelo índice recebido
+		return cedulas[i];
 	}
 	
+/**
+ * Metodo para saque de cédulas. Os parametos de entrada são de qual cédula sera sacado,
+ * e quantas unidades serao sacadas
+ */
 	public void SacarNota (int i, int qnt) {
-		cedulas[i]=cedulas[i]-qnt;				//Método para saque de cédulas, onde recebe de qual cédula sera sacado, e quantas unidades serão sacadas
+		cedulas[i]=cedulas[i]-qnt;
 	}
-	public int SaldoCaixa() {					//Método para exibir qual o total de dinheiro contido no caixa
+
+/**
+ * Método para exibir qual o total de dinheiro contido no caixa
+ */
+	
+	public int SaldoCaixa() {					//
 		int saldo;
 		saldo = (cedulas[0]*10)+(cedulas[1]*20)+(cedulas[2]*50)+(cedulas[3]*100);
 		return saldo;
 	}
+	
+
+/**
+ * Método para exibir o total de unidades de cada cedula contidas no caixa
+ */	
+	
 	public void MostrarQuantidades () {
 		System.out.print("O saldo total no caixa é: ");System.out.println(SaldoCaixa());
 		System.out.println("Valor:\t\t|\tR$10\t|\tR$20\t|\tR$50\t|\tR$100\t|");
